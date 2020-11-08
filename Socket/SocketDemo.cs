9fb08a017107515169e6dd304a5fc42b8243f35a
zhdquest <48425796+zhdquest@users.noreply.github.com>
2020-11-08 19:26:09 +0800

    Socket

---
+++ Socket/SocketDemo.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using UnityEngine;
+
+public class SocketDemo : MonoBehaviour
+{
+    public GUISkin skin;
+    
+    private string serverIP = "127.0.0.1";
+    private string serverPort = "12345";
+    //消息列表
+    private string log = "";
+    //要发送的消息
+    private string sendMsg = "";
+
+    private int currentState = 0;
+
+    private void Awake()
+    {
+        Application.runInBackground = true;
+    }
+
+    private void OnGUI()
+    {
+        //设置皮肤
+        GUI.skin = skin;
+        
+        //【未联网状态】
+        if (currentState == 0)
+        {
+            GUILayout.Label("请输入服务端IP:");
+            serverIP = GUILayout.TextField(serverIP);
+            GUILayout.Label("请输入服务端Port:");
+            serverPort = GUILayout.TextField(serverPort);
+
+            if (GUILayout.Button("创建服务器"))
+            {
+                //创建服务器，服务器收到的回调消息，传输给Log去显示
+                SocketTool.GetInstance().ServerInit(msg =>
+                {
+                    log += msg + "\n";
+                });
+                //改变状态为【服务器状态】
+                currentState = 1;
+            }
+
+            if (GUILayout.Button("连接服务器"))
+            {
+                SocketTool.GetInstance().ClientConnect(
+                    serverIP,int.Parse(serverPort),
+                    msg =>
+                    {
+                        log += msg + "\n";
+                    });
+                //改变状态为【客户端状态】
+                currentState = 2;
+            }
+        }
+        //【服务器状态】
+        else if(currentState == 1)
+        {
+            //暂无操作
+        }
+        //【客户端状态】
+        else if (currentState == 2)
+        {
+            GUILayout.Label("请输入要发送的消息：");
+            sendMsg = GUILayout.TextField(sendMsg);
+
+            if (GUILayout.Button("发送..."))
+            {
+                //客户端发送消息
+                SocketTool.GetInstance().ClientSend(sendMsg);
+            }
+        }
+
+        //显示日志
+        GUILayout.Label(log);
+    }
+
+    private void OnApplicationQuit()
+    {
+        //收尾
+        SocketTool.GetInstance().SocketDispose();
+    }
+}
---
+++ Socket/SocketTool.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+public class SocketTool
+{
+    #region Singleton
+    private static SocketTool instance;
+    public static SocketTool GetInstance()
+    {
+        if (instance == null)
+        {
+            instance = new SocketTool();
+        }
+        return instance;
+    }
+    private SocketTool()
+    {
+    }
+    #endregion
+
+    #region Server
+
+    //服务端套接字
+    private Socket serverSocket;
+    //服务端上层回调
+    private Action<string> serverCallback;
+    //已经连接的客户端
+    private Dictionary<IPAddress, Socket> connectedClient;
+    //服务端消息缓存
+    private byte[] serverBuffer;
+
+    /// <summary>
+    /// 初始化服务端
+    /// </summary>
+    public void ServerInit(Action<string> serverCallback)
+    {
+        //设置委托回调
+        this.serverCallback = serverCallback;
+        //实例化字典
+        connectedClient = new Dictionary<IPAddress, Socket>();
+        //实例化缓存长度
+        serverBuffer = new byte[1024];
+        //实例化Socket对象【TCP】
+        serverSocket = new Socket(
+            AddressFamily.InterNetwork,/*地址族：IPV4*/
+            SocketType.Stream,/*双向读写流*/
+            ProtocolType.Tcp/*传输层协议类型*/);
+        //实例化Socket对象【UDP】
+        // serverSocket = new Socket(
+        //     AddressFamily.InterNetwork,/*地址族：IPV4*/
+        //     SocketType.Dgram,/*数据报*/
+        //     ProtocolType.Udp/*传输层协议类型*/);
+
+        serverCallback("服务端创建完毕...");
+        //创建IP网络结点
+        IPEndPoint ipEndPoint = new IPEndPoint(IPAddress.Any,12345);
+        //绑定网络结点
+        serverSocket.Bind(ipEndPoint);
+        serverCallback("网络结点绑定完毕...");
+        //设置监听队列
+        serverSocket.Listen(5);
+        //异步接受客户端的连接请求
+        serverSocket.BeginAccept(ServerAccept, serverSocket);
+        serverCallback("异步接受请求开始...");
+    }
+
+    /// <summary>
+    /// 异步接受请求后，接受到客户端的回调
+    /// </summary>
+    /// <param name="ar"></param>
+    private void ServerAccept(IAsyncResult ar)
+    {
+        //获取到异步接受的监听
+        Socket listener = ar.AsyncState as Socket;
+        //结束异步接受
+        Socket acceptClient = listener.EndAccept(ar);
+        //获取该客户端的本地地址
+        IPAddress clientLocalAddress = (acceptClient.LocalEndPoint 
+            as IPEndPoint).Address;
+        //将当前客户端的Socket保存
+        connectedClient.Add(clientLocalAddress,acceptClient);
+        serverCallback("异步接受请求结束..." + acceptClient.LocalEndPoint);
+        //开始对该客户端进行异步的消息接收
+        acceptClient.BeginReceive(serverBuffer, /*消息缓存*/
+            0, /*消息接收的偏移量*/
+            serverBuffer.Length, /*消息接收长度*/
+            SocketFlags.None, /*消息接收的特殊标记*/
+            ServerReceive, /*消息接受到后的回调*/
+            acceptClient/*状态信息*/);
+        serverCallback("异步消息接收开始...");
+        //尾递归【继续接受其他客户端的连接请求】
+        serverSocket.BeginAccept(ServerAccept, serverSocket);
+    }
+
+    /// <summary>
+    /// 异步接收消息的回调
+    /// </summary>
+    /// <param name="ar"></param>
+    private void ServerReceive(IAsyncResult ar)
+    {
+        //获取异步状态
+        Socket listener = ar.AsyncState as Socket;
+        //收到消息的字节数
+        int count = listener.EndReceive(ar);
+        serverCallback("异步消息接收结束，接收了..." + count + "个字节...");
+        //将收到的字节流转换成字符串
+        string msg = UTF8Encoding.UTF8.GetString(serverBuffer);
+        serverCallback("接收到的消息为..." + msg);
+        //清空字节流
+        serverBuffer = new byte[1024];
+        //尾递归
+        listener.BeginReceive(serverBuffer, /*消息缓存*/
+            0, /*消息接收的偏移量*/
+            serverBuffer.Length, /*消息接收长度*/
+            SocketFlags.None, /*消息接收的特殊标记*/
+            ServerReceive, /*消息接受到后的回调*/
+            listener/*状态信息*/);
+    }
+
+    #endregion
+
+    #region Client
+
+    //客户端套接字
+    private Socket clientSocket;
+    //客户端缓存
+    private byte[] clientBuffer;
+    //客户端消息回调
+    private Action<string> clientCallback;
+
+    /// <summary>
+    /// 客户端连接
+    /// </summary>
+    public void ClientConnect(string serverIP,
+        int serverPort,Action<string> clientCallback)
+    {
+        //设置回调
+        this.clientCallback = clientCallback;
+        //实例化Socket
+        clientSocket = new Socket(
+            AddressFamily.InterNetwork,
+            SocketType.Stream,ProtocolType.Tcp);
+
+        clientCallback("客户端创建完毕..");
+        //组装网络结点
+        IPEndPoint ipEndPoint = new IPEndPoint(
+            IPAddress.Parse(serverIP),serverPort);
+        //连接服务器
+        clientSocket.BeginConnect(ipEndPoint, ClientConnected, clientSocket);
+        clientCallback("客户端开始异步连接服务器..");
+    }
+
+    /// <summary>
+    /// 客户端连接成功
+    /// </summary>
+    /// <param name="ar"></param>
+    private void ClientConnected(IAsyncResult ar)
+    {
+        Socket listener = ar.AsyncState as Socket;
+        //结束连接
+        listener.EndConnect(ar);
+        clientCallback("客户端连接服务器成功!..");
+    }
+
+    /// <summary>
+    /// 客户端发送消息
+    /// </summary>
+    /// <param name="msg"></param>
+    public void ClientSend(string msg)
+    {
+        //将消息转换为字节数组，保存到buffer
+        clientBuffer = UTF8Encoding.UTF8.GetBytes(msg);
+        //发消息
+        clientSocket.BeginSend(clientBuffer, 0,
+            clientBuffer.Length, SocketFlags.None,
+            ClientSendSuccess,clientSocket);
+        clientCallback("客户端向服务端发送消息.." + msg);
+    }
+
+    private void ClientSendSuccess(IAsyncResult ar)
+    {
+        Socket listener = ar.AsyncState as Socket;
+        //结束发送
+        int count = listener.EndSend(ar);
+        clientCallback("客户端成功发送消息" + count + "个字节...");
+    }
+
+    #endregion
+
+    #region Socket Dispose
+
+    /// <summary>
+    /// 收尾操作
+    /// </summary>
+    public void SocketDispose()
+    {
+        if (serverSocket != null)
+        {
+            serverSocket.Close();
+            serverSocket = null;
+        }
+
+        if (clientSocket != null)
+        {
+            clientSocket.Disconnect(true);
+            clientSocket.Close();
+            clientSocket = null;
+        }
+    }
+
+    #endregion
+
+}
